63247e96281825d6cb9ce7dbe307cf5cee79536f
Mulaym SIngh Yadav <rituraj.pandey786@gmail.com>
2020-03-04 14:43:44 +0530

    Worked on Ui design

---
+++ MonicaLoanApp/MonicaLoanApp/Views/Loans/YourLoanBalancePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using MonicaLoanApp.ViewModels.Loans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MonicaLoanApp.Views.Loans
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class YourLoanBalancePage : ContentPage
+    {
+        //Define Properties Here...
+        protected YourLoanBalancePageVM YourLoanBalancePageVM;
+        public YourLoanBalancePage()
+        {
+            InitializeComponent();
+            YourLoanBalancePageVM = new YourLoanBalancePageVM(this.Navigation);
+            BindingContext = YourLoanBalancePageVM;
+        }
+    }
+}

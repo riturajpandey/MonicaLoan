5ec8f3926001a2a20b040c9aa678b6057fb9511c
Ashwini Bokade <ashwini_bokade@gmail.com>
2020-03-05 14:18:58 +0530

    new commit

---
+++ MonicaLoanApp/MonicaLoanApp/ViewModels/MyAccount/AddressPageVM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MonicaLoanApp.ViewModels.MyAccount
+{
+    public class AddressPageVM : BaseViewModel
+    {
+        #region Constructor
+        public AddressPageVM(INavigation nav)
+        {
+            Navigation = nav;
+        }
+        #endregion
+
+        #region Properties
+        private string _AddressOne;
+        public string AddressOne
+        {
+            get { return _AddressOne; }
+            set
+            {
+                if (_AddressOne != value)
+                {
+                    _AddressOne = value;
+                    OnPropertyChanged("AddressOne");
+                }
+            }
+            #endregion
+
+            #region Commands
+            #endregion
+
+            #region Methods
+            #endregion
+        }
+    }
+}
